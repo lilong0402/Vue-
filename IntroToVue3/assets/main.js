9a9e36ce5749f31fc7220cc000703b649e348bdf
unknown <2199630705@qq.com>
2023-03-10 09:47:31 +0800

    Attribute binding &Conditional Rendering

---
+++ IntroToVue3/assets/main.js
@@ -0,0 +1,12 @@
+// const product='Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './images/socks_green.jpg',
+            inStock: true 
+            // url:''
+        }
+    }
+})
