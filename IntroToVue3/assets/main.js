6b44fd6b7709e4f7cd08c676f5182e5068b8f6ca
unknown <2199630705@qq.com>
2023-03-10 11:05:51 +0800

    云班课练习

--- IntroToVue3/assets/main.js
+++ IntroToVue3/assets/main.js
@@ -5,7 +5,34 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './images/socks_green.jpg',
-            inStock: true 
+            inStock: true ,
+            button : 0,
+            counts : [
+                {
+                    id : 1,
+                    courtName: '后端工程化开发',
+                    className : '软件2232',
+                    classId: 12121,
+                    Time : '2022-2023-2',
+                    image : '../images/onejpeg.jpeg',
+                },
+                {
+                    id : 2,
+                    courtName: '前端工程化开发',
+                    className : '软件2232',
+                    classId: 12122,
+                    Time : "2022-2023-2",
+                    image : '../images/twojpg.jpg',
+                },
+                {
+                    id : 3,
+                    courtName: 'web应用开发',
+                    className : '软件2232',
+                    classId: 12123,
+                    Time : '2022-2023-2',
+                    image : '../images/threejpeg.jpeg',
+                }
+            ]
             // url:''
         }
     }
